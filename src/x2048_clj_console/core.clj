e160d96dc6c58c82385a6861ca2e1379f635a584
lukealba <lalbarracin2000@gmail.com>
2018-07-31 15:42:24 -0500

    Added core logic.  Intermediate state. Still need to complete other vector dimensions.

--- src/x2048_clj_console/core.clj
+++ src/x2048_clj_console/core.clj
@@ -1,17 +1,41 @@
 ;; core.clj
 (ns x2048-clj-console.core)
 
-(defn print-grid [grid]
-  (partition 4 @grid)
-)
+(def grid (atom (repeat 16 2)))
 
-(defn move-grid [new-grid index]
-    (let [foo (assoc (into [] (print-grid)) index 
-    (declare iterate-thru)
-    (iterate-thru (drop-while zero? (nth (print-grid) index))0))]
-      (if (< index 3)
-        (recur foo (inc index))
-        foo)))
+(defn print-grid []
+  (partition 4 @grid))
+
+(defn print-grid-vec [nums index] ;;(shift-grid (print-grid vec [] 0)0)
+  (if (< index 3)
+    (recur (conj nums (into [](nth (print-grid) index))) (inc index))
+    (conj nums (into [] (nth (print-grid) index)))))
+
+;;rotate-grid would be shift-up rotating in 270 or -90 degrees
+(defn rotate-grid [nums index] ;;2d vector of the columns or back to rows
+  (if (< index 3) ;;change @grid and add parameter to allow rotation
+    (recur (conj nums (into [](take-nth 4 (drop index @grid)))) (inc index))
+    (conj nums (into [] (take-nth 4 (drop index @grid))))))
+
+(defn flip-grid [nums index] ;;change out print-grid-vec into params
+  (if (< index 3)
+    (recur (conj nums (into [](reverse (nth (print-grid-vec [] 0) index)))) (inc index))
+    (conj nums (into [] (reverse (nth (print-grid-vec [] 0) index))))))
+
+(defn add-zeros [nums]
+ (concat nums (take (- 4 (count nums)) (cycle (range 0 1)))))
+
+(defn filter-zeros [nums]
+  (add-zeros (filter pos? nums))) 
+
+(defn shift-grid [new-grid index]
+  (if (< index 3)
+      (recur (vec-algorithm new-grid index) (inc index))
+      (vec-algorithm new-grid index)))
+
+(defn vec-algorithm [new-grid index]
+  (assoc new-grid index
+  (iterate-thru (into [] (drop-while zero? (nth (print-grid) index)))0)))
 
 (defn iterate-thru [nums index] ;;recursion
   (if (> (- (count nums) 1) index)
@@ -19,42 +43,12 @@
     (declare num-compare)
     (let [foo (num-compare (nth nums index) (nth nums (+ index 1)) nums index)]
       (recur foo (inc index))))
-  nums)) ;;false returns nums
+  (filter-zeros nums))) ;;false returns nums
 
 (defn num-compare [x y nums index] ;;x is first, y is second
   (if (= x y)
     (update (update nums (+ index 1) #(* % 0)) index #(* % 2))
-    nums
-  )
-)
-
-(comment "
-(defn print-columns 
-  ([] (let [foo (repeat 4 (list nil nil nil nil))] 
-  (recur (print-grid grid))))
-  ([new-grid]
-    (let [foo (list (loop-columns (range 0 4)))]
-    foo)))
- 
-(defn loop-columns [index]
-  (filter (= (sort-for-columns (range 0 16)) index))
-)
-(defn pair-column-with-index [index num] ;;num = new index
-  (cond num
-    (> 1 (/ index 4)) 0
-    (> 2 (/ index 4)) 1
-    (> 3 (/ index 4)) 2
-    : else 3))
-
-(defn sort-for-columns [index] 
-  (cond
-    (zero? (mod index 4)) 0
-    (zero? (mod (- index 1)4)) 1
-    (zero? (mod (- index 2)4)) 2
-    :else 3))
-")
+    nums))
 
 (defn -main [& args]
-  (println "Hello, 2048! :)")
-  ;;(def grid (repeat 4 (list 0 0 0 0)))
-  (def grid (atom(repeat 16 2))))
+  (println "Hello, 2048! :)"))
