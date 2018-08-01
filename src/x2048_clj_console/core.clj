b539fc1901b004f25e37e5e745c9472848fa4447
lukealba <lalbarracin2000@gmail.com>
2018-08-01 11:13:40 -0500

    Finalized dproject. Allows for continuous gameplay.

--- src/x2048_clj_console/core.clj
+++ src/x2048_clj_console/core.clj
@@ -1,26 +1,29 @@
 ;; core.clj
 (ns x2048-clj-console.core)
 
-(def grid (atom (repeat 16 2)))
+(def grid (atom (repeat 16 0)))
+(def score (atom 0))
 
 (defn print-grid []
   (partition 4 @grid))
 
-(defn print-grid-vec [nums index] ;;(shift-grid (print-grid vec [] 0)0)
+(defn print-grid-vec [nums index] ;;(shift-grid (print-grid-vec [] 0)0)
   (if (< index 3)
     (recur (conj nums (into [](nth (print-grid) index))) (inc index))
     (conj nums (into [] (nth (print-grid) index)))))
 
-;;rotate-grid would be shift-up rotating in 270 or -90 degrees
-(defn rotate-grid [nums index] ;;2d vector of the columns or back to rows
+;;rotate-grid would be shift-up rotating in -270 or 90 degrees
+;; use @grid for up
+(defn rotate-grid [nums index temp-grid] ;;2d vector of the columns or back to rows
   (if (< index 3) ;;change @grid and add parameter to allow rotation
-    (recur (conj nums (into [](take-nth 4 (drop index @grid)))) (inc index))
-    (conj nums (into [] (take-nth 4 (drop index @grid))))))
+    (recur (conj nums (into [](take-nth 4 (drop index temp-grid)))) (inc index) temp-grid)
+    (conj nums (into [] (take-nth 4 (drop index temp-grid))))))
 
-(defn flip-grid [nums index] ;;change out print-grid-vec into params
+;;use (print-grid-vec [] 0) for right, (rotate-grid ) for left
+(defn flip-grid [nums index temp-grid] ;;change out print-grid-vec into params
   (if (< index 3)
-    (recur (conj nums (into [](reverse (nth (print-grid-vec [] 0) index)))) (inc index))
-    (conj nums (into [] (reverse (nth (print-grid-vec [] 0) index))))))
+    (recur (conj nums (into [](reverse (nth temp-grid index)))) (inc index) temp-grid)
+    (conj nums (into [] (reverse (nth temp-grid index))))))
 
 (defn add-zeros [nums]
  (concat nums (take (- 4 (count nums)) (cycle (range 0 1)))))
@@ -28,27 +31,70 @@
 (defn filter-zeros [nums]
   (add-zeros (filter pos? nums))) 
 
+(defn num-compare [x y nums index] ;;x is first, y is second
+  (if (= x y) ;;doubles the first number and sets second number to 0 if true
+    (do
+      (reset! score (+ (* 2 x) @score))
+      (update (update nums (+ index 1) #(* % 0)) index #(* % 2))) 
+    nums))
+
+(defn iterate-thru [nums index] ;;recursion
+  (if (> (- (count nums) 1) index)
+    (do ;;true
+      (let [foo (num-compare (nth nums index) (nth nums (+ index 1)) nums index)]
+        (recur foo (inc index))))
+    (filter-zeros nums)))
+
+(defn vec-algorithm [new-grid index]
+  (assoc new-grid index
+         (iterate-thru (into [] (filter pos? (nth new-grid index))) 0)))
+
 (defn shift-grid [new-grid index]
+  (declare vec-algorithm)
   (if (< index 3)
       (recur (vec-algorithm new-grid index) (inc index))
       (vec-algorithm new-grid index)))
 
-(defn vec-algorithm [new-grid index]
-  (assoc new-grid index
-  (iterate-thru (into [] (drop-while zero? (nth (print-grid) index)))0)))
+(defn rotate-none [] ;;shift-left
+  (into [] (print-grid)))
 
-(defn iterate-thru [nums index] ;;recursion
-  (if (> (- (count nums) 1) index)
-  (do ;;true
-    (declare num-compare)
-    (let [foo (num-compare (nth nums index) (nth nums (+ index 1)) nums index)]
-      (recur foo (inc index))))
-  (filter-zeros nums))) ;;false returns nums
+(defn rotate-half [] ;;shift-right 
+  (flip-grid [] 0 (print-grid-vec [] 0)))
 
-(defn num-compare [x y nums index] ;;x is first, y is second
-  (if (= x y)
-    (update (update nums (+ index 1) #(* % 0)) index #(* % 2))
-    nums))
+(defn rotate-small [] ;;-90 or 270 shift-down
+  (flip-grid [] 0 (rotate-grid [] 0 @grid)))
+
+(defn rotate-large [] ;;-270 or 90 shift-up
+  (rotate-grid [] 0 @grid))
+
+(defn add-block []
+  (let [foo (rand-int 16)]
+  (if (zero?(nth @grid foo))
+    (reset! grid (assoc (into [] @grid) foo 2))
+    (if (zero?(count (filter zero? (into [] @grid))))
+      (println "Be very careful with your next move...")
+      (recur)))))
+
+(defn display-grid []
+  (let [input (read-line)]
+      (cond
+        (= input "w") (reset! grid (flatten (rotate-grid [] 0 (flatten (shift-grid (rotate-large) 0)))))
+        (= input "a") (reset! grid (flatten (shift-grid (rotate-none) 0)))
+        (= input "s") (reset! grid (flatten (rotate-grid [] 0 (flatten (flip-grid [] 0 (shift-grid (rotate-small) 0))))))
+        :else (reset! grid (flatten (flip-grid [] 0 (shift-grid (rotate-half) 0))))))
+  (add-block)
+  (doseq [foo (print-grid)] (println foo))
+  (println "Score : " @score)
+  (recur))
 
 (defn -main [& args]
-  (println "Hello, 2048! :)"))
+  (println "Hello, 2048! :)")
+  (add-block)
+  (add-block)
+  (display-grid))
+  
+      
+
+
+
+
--- test/x2048_clj_console/core_test.clj
+++ test/x2048_clj_console/core_test.clj
@@ -6,44 +6,4 @@
   (testing "FIXME, I fail."
     (is (= 0 1))))
 
-"
-(defn shift-grid [new-grid index]
-     (cond index
-    (= index 0) (recur (vec-algorithm print-grid-vec index) (inc index))
-    (< index 3) (recur (vec-algorithm new-grid index) (inc index))
-      :else (vec-algorithm new-grid index)
-     )"
-(comment "
-(defn move-grid [new-grid index]
-  (let [foo (assoc (into [] (print-grid)) index
-                   (declare iterate-thru)
-                   (iterate-thru (into [] (drop-while zero? (nth (print-grid-vec) index))) 0))]
-    (if (< index 3)
-      (recur foo (inc index))
-      (add-zeros foo))))")
 
-(comment "
-(defn print-columns 
-  ([] (let [foo (repeat 4 (list nil nil nil nil))] 
-  (recur (print-grid grid))))
-  ([new-grid]
-    (let [foo (list (loop-columns (range 0 4)))]
-    foo)))
- 
-(defn loop-columns [index]
-  (filter (= (sort-for-columns (range 0 16)) index))
-)
-(defn pair-column-with-index [index num] ;;num = new index
-  (cond num
-    (> 1 (/ index 4)) 0
-    (> 2 (/ index 4)) 1
-    (> 3 (/ index 4)) 2
-    : else 3))
-
-(defn sort-for-columns [index] 
-  (cond
-    (zero? (mod index 4)) 0
-    (zero? (mod (- index 1)4)) 1
-    (zero? (mod (- index 2)4)) 2
-    :else 3))
-")
